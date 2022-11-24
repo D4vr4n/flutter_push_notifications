70ef6dc7ed63889802ba7f7bd691be9d7ec6e5ba
Davran Arifzhanov <davron2001davron@gmail.com>
2022-11-24 16:04:59 +0600

    local notifications scheduling

--- lib/src/push_handler.dart
+++ lib/src/push_handler.dart
@@ -5,6 +5,7 @@ import 'dart:math';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:l/l.dart';
+import 'package:notifications/src/local_notifications_service.dart';
 import 'package:notifications/src/push_navigator_holder.dart';
 import 'package:rxdart/subjects.dart';
 
@@ -12,10 +13,16 @@ import 'base_messaging_service.dart';
 import 'push_handle_strategy.dart';
 import 'push_handle_strategy_factory.dart';
 
+const String pushIdParam = 'push_id_param';
+
 /// Класс для управления уведомлениями
 class PushHandler {
+  final PushHandleStrategyFactory _strategyFactory;
+  final BaseMessagingService _messagingService;
+  final LocalNotificationsService _localNotificationsService;
+
   /// Инициализация уведомлений происходит здесь при создании
-  PushHandler(this._messagingService, this._strategyFactory, this._localNotificationsPlugin) {
+  PushHandler(this._messagingService, this._strategyFactory, this._localNotificationsService) {
     initialize();
   }
 
@@ -24,15 +31,13 @@ class PushHandler {
 
   final BehaviorSubject<PushHandleStrategy<dynamic>> selectNotificationSubject = BehaviorSubject();
 
-  final PushHandleStrategyFactory _strategyFactory;
-  final BaseMessagingService _messagingService;
-  final FlutterLocalNotificationsPlugin _localNotificationsPlugin;
-
   late Random random;
 
   Future<void> initialize() async {
-    /// Игициализация сервиса
+    /// Инициализация сервисов
     await _messagingService.initNotifications(handleMessage);
+
+    await _localNotificationsService.init();
 
     /// Запрос разрешений на отправку уведомлений
     if (Platform.isIOS) {
@@ -49,16 +54,10 @@ class PushHandler {
   }) {
     final PushHandleStrategy<dynamic> strategy = _strategyFactory.createByData(message);
 
-    /// Настройки уведомлений
-    final AndroidInitializationSettings androidInit = AndroidInitializationSettings(
-      strategy.icon ?? '@mipmap/ic_launcher_foreground',
-    );
-    const IOSInitializationSettings iosInit = IOSInitializationSettings();
-
     /// Детали уведомлений
     final AndroidNotificationDetails androidDetails = AndroidNotificationDetails(
-      strategy.notificationChannelId ?? 'mak_it_local_id',
-      strategy.notificationChannelName ?? 'mak_it_local_name',
+      strategy.notificationChannelId ?? 'Zam Zam Local ID',
+      strategy.notificationChannelName ?? 'Zam Zam Local Name',
       icon: strategy.icon,
       priority: Priority.high,
       importance: Importance.high,
@@ -68,26 +67,7 @@ class PushHandler {
       ongoing: strategy.ongoing,
     );
     const IOSNotificationDetails iosDetails = IOSNotificationDetails();
-    final NotificationDetails details = NotificationDetails(
-      android: androidDetails,
-      iOS: iosDetails,
-    );
-
-    /// Настройки ициниализации локального показа уведомлений
-    final InitializationSettings initializationSettings = InitializationSettings(
-      android: androidInit,
-      iOS: iosInit,
-    );
-
-    /// Инициализация локальных уведомлений
-    _localNotificationsPlugin.initialize(
-      initializationSettings,
-      onSelectNotification: (String? value) {
-        l.d('VALUE SELECTED: $value');
-        selectNotificationSubject.add(strategy);
-        strategy.onTapNotification(PushNavigatorHolder().navigator);
-      },
-    );
+    final NotificationDetails details = NotificationDetails(android: androidDetails, iOS: iosDetails);
 
     if (!localNotification) {
       messageSubject.add(message.data);
@@ -101,13 +81,21 @@ class PushHandler {
       /// Создаем [id] для показа уведомления
       final int pushId = random.nextInt((pow(2, 31) - 1).toInt());
 
+      final payload = jsonEncode(message.data..addAll({pushIdParam: pushId}));
+
       /// Показывем уведомление локально
-      _localNotificationsPlugin.show(
+      _localNotificationsService.show(
         pushId,
         strategy.payload.title,
         strategy.payload.body,
         details,
-        payload: jsonEncode(message.data),
+        payload: payload,
+        onSelectNotification: (payload) {
+          l.d('VALUE SELECTED: $payload');
+
+          selectNotificationSubject.add(strategy);
+          strategy.onTapNotification(PushNavigatorHolder().navigator);
+        },
       );
     }
   }
